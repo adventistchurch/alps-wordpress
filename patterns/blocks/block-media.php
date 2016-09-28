3e0eb4a30aaf1e597b0ce009d66a4db26fcd1b5b
Terrance Pitre <tpitre@southleft.com>
2016-09-28 15:57:37 -0500

    Adding in the two-column layout for page/article footer blocks.

--- patterns/blocks/block-media.php
+++ patterns/blocks/block-media.php
@@ -2,7 +2,7 @@
   <?php if ($kicker): ?>
     <span class="kicker font--secondary--m upper theme--secondary-text-color db"><?php echo $kicker ?></span>
   <?php endif; ?>
-  <div class="media-block__inner spacing--quarter block__row">
+  <div class="media-block__inner spacing--quarter <?php echo ($block_inner_class) ? $block_inner_class : 'block__row'; ?>">
     <?php if ($thumbnail && $button_url): ?>
       <a class="media-block__image-wrap block__image-wrap db" href="<?php echo $button_url; ?>">
         <div class="<?php if ($round_image): ?>round<?php endif; ?>  dib"><img class="media-block__image block__image" src="<?php echo $thumbnail; ?>" alt="<?php echo $alt; ?>" /></div>
