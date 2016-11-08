7ff6a82e1c52f65f282ded931a7c7d55a0776969
Kelsey Cahill <kcahill@southleft.com>
2016-11-08 11:52:20 -0500

    Added function to require plugins on theme install

--- functions.php
+++ functions.php
@@ -28,6 +28,51 @@ foreach ($sage_includes as $file) {
 unset($file, $filepath);
 
 /**
+ * Require plugins on theme install
+ */
+require_once get_template_directory() . 'lib/plugin-activation.php';
+add_action( 'tgmpa_register', 'adventist_register_required_plugins' );
+function adventist_register_required_plugins() {
+	$plugins = array(
+		array(
+			'name'               => 'Advanced Custom Fields', // The plugin name.
+			'slug'               => 'advanced-custom-fields', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/lib/plugins/advanced-custom-fields.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+		),
+    array(
+			'name'               => 'Advanced Custom Fields Pro',
+			'slug'               => 'advanced-custom-fields-pro',
+			'source'             => get_template_directory() . '/lib/plugins/advanced-custom-fields-pro.zip',
+			'required'           => true,
+		array(
+			'name'        => 'WordPress SEO by Yoast',
+			'slug'        => 'wordpress-seo',
+      'source'             => get_template_directory() . '/lib/plugins/wordpress-seo.zip',
+			'required'           => true,
+			'is_callable' => 'wpseo_init',
+		),
+	);
+
+	$config = array(
+		'id'           => 'adventist',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+	tgmpa( $plugins, $config );
+}
+
+/**
  * Register sidebar navigation
  */
 function register_my_menus() {
