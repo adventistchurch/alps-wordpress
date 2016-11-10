3799390ee9b467949717e60b3382d3add9c5638a
Kelsey Cahill <kcahill@southleft.com>
2016-11-10 17:34:56 -0500

    Fix for Bug #9

--- functions.php
+++ functions.php
@@ -178,15 +178,14 @@ add_action( 'init', 'register_my_menus' );
 /**
  * Hide content area on 'Single' page templates
  */
-add_action( 'admin_init', 'hide_editor' );
 function hide_editor() {
   $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
-  if ( !isset( $post_id ) ) return;
   $template_file = get_post_meta($post_id, '_wp_page_template', true);
-  if ($template_file == 'template-single.php'){ // edit the template name
-      remove_post_type_support('page', 'editor');
+  if ($template_file == 'template-single.php') { // edit the template name
+    remove_post_type_support('page', 'editor');
   }
 }
+add_action( 'admin_head', 'hide_editor' );
 
 /**
  * Function to add classes to Prev & Next pagination links
