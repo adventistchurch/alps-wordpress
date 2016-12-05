f46bd167384b4043e878ef73b7a62aa21ef9e9a8
Terrance Pitre <tpitre@southleft.com>
2016-12-05 15:42:34 -0600

    Comment updates.

--- templates/comments.php
+++ templates/comments.php
@@ -8,7 +8,7 @@ if (post_password_required()) {
   <?php if (have_comments()) : ?>
     <h2 class="font--secondary--l"><?php printf(_nx('One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sage'), number_format_i18n(get_comments_number()), '<span>' . get_the_title() . '</span>'); ?></h2>
 
-    <ol class="comment-list">
+    <ol class="comment-list spacing">
       <?php wp_list_comments(['style' => 'ol', 'short_ping' => true]); ?>
     </ol>
 
