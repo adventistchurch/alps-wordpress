d6969314ff99458119aa4e1f386d4e8d4b01df87
Kelsey Cahill <kcahill@southleft.com>
2017-01-03 10:56:53 -0500

    Added dropdown for feed category

--- piklist/parts/widgets/widget-post-feed-form.php
+++ piklist/parts/widgets/widget-post-feed-form.php
@@ -1,19 +1,35 @@
 <?php
   piklist('field', array(
+    'type' => 'select',
+    'field' => 'feed_category_list',
+    'label' => 'Category',
+    'description' => 'Select the category for the feed.',
+    'value' => 'news',
+    'choices' => piklist(
+      get_terms('category', array(
+        'hide_empty' => true
+      )),
+      array(
+        'term_id',
+        'name'
+      )
+    )
+  ));
+  piklist('field', array(
     'type' => 'text',
-    'field' => 'news_feed_title',
+    'field' => 'feed_title',
     'label' => 'Custom Feed Title',
     'columns' => 6
   ));
   piklist('field', array(
     'type' => 'text',
-    'field' => 'news_widget_post_count',
+    'field' => 'feed_widget_post_count',
     'label' => 'Number of posts',
     'columns' => 2
   ));
   piklist('field', array(
     'type' => 'checkbox',
-    'field' => 'news_sidebar',
+    'field' => 'for_sidebar',
     'label' => 'Sidebar',
     'choices' => array(
       'true' => 'Check this box to format the feed for the sidebar.'
@@ -21,13 +37,13 @@
   ));
   piklist('field', array(
     'type' => 'text',
-    'field' => 'news_widget_btn_text',
+    'field' => 'feed_widget_btn_text',
     'label' => 'More Button Text',
     'columns' => 6
   ));
   piklist('field', array(
     'type' => 'text',
-    'field' => 'news_widget_btn_link',
+    'field' => 'feed_widget_btn_link',
     'label' => 'More Button Link',
     'columns' => 6
   ));
