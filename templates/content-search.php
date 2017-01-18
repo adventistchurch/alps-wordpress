5ebe407faec42e3bec9bd84451bd2fad2693dc43
Kelsey Cahill <kcahill@southleft.com>
2017-01-18 15:10:27 -0500

    Removed narrowed terms from search page

--- templates/content-search.php
+++ templates/content-search.php
@@ -3,7 +3,6 @@
   $thumb_id = get_post_thumbnail_id();
   // Image alt
   $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
-  $post_id_array = array();
 ?>
 
 <div class="layout-container full--until-large">
@@ -20,26 +19,6 @@
         <div class="search__results spacing--one-and-half">
           <div class="search__options pad--primary no-pad--btm spacing--half">
             <?php get_template_part('patterns/components/search-form-page'); ?>
-            <div class="search__filters spacing--half">
-              <h3 class="font--secondary--m upper">Narrow Search</h3>
-              <?php
-                $terms = get_terms(
-                  'category', array(
-                  'parent'   => 0,
-                  'hide_empty' => false // Optional
-                  )
-                );
-              ?>
-              <?php if (count($terms) > 0): ?>
-                <ul class="bullet-list spacing--quarter theme--primary-text-color">
-                  <?php foreach ($terms as $term): ?>
-                    <li class="font--primary--s">
-                      <a href="<?php echo home_url( '/' ); ?>?s=<?php echo get_search_query(); ?><?php echo esc_html('&cat=' . $term->term_id); ?>" title="<?php echo esc_attr($term->name); ?>"><?php echo esc_html($term->name); ?></a>
-                    </li>
-                  <?php endforeach; ?>
-                </ul>
-              <?php endif; ?>
-            </div> <!-- /.search__filters -->
           </div> <!-- /.search__options -->
           <hr>
           <div class="spacing--one-and-half">
