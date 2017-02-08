61f6b8e1c9b4f01aaa315da758f2f92d341b225a
Kelsey Cahill <kcahill@southleft.com>
2017-02-08 16:51:08 -0500

    Fix for #81

--- patterns/blocks/block-media.php
+++ patterns/blocks/block-media.php
@@ -1,6 +1,12 @@
 <div class="media-block block spacing--quarter">
   <?php if (isset($kicker) && !empty($title)): ?>
-    <span class="kicker font--secondary--m upper theme--secondary-text-color db"><a href="<?php echo $button_url; ?>"><?php echo $kicker; ?></a></span>
+    <span class="kicker font--secondary--m upper theme--secondary-text-color db">
+      <?php if ($button_url): ?>
+        <a href="<?php echo $button_url; ?>"><?php echo $kicker; ?></a>
+      <?php else: ?>
+        <?php echo $kicker; ?>
+      <?php endif; ?>
+    </span>
   <?php endif; ?>
   <div class="media-block__inner spacing--quarter <?php echo ($block_inner_class) ? $block_inner_class : 'block__row'; ?>">
     <?php if ($thumbnail && $button_url): ?>
@@ -9,9 +15,21 @@
           <img class="media-block__image block__image" src="<?php if ($round_image == 'true'): echo $thumbnail_round; else: echo $thumbnail; endif; ?>" alt="<?php echo $alt; ?>" />
         </div>
       </a> <!-- /.media-block__image-wrap -->
+    <?php else: ?>
+      <div class="media-block__image-wrap block__image-wrap db">
+        <div class="<?php if ($round_image == 'true'): ?>round<?php endif; ?> dib">
+          <img class="media-block__image block__image" src="<?php if ($round_image == 'true'): echo $thumbnail_round; else: echo $thumbnail; endif; ?>" alt="<?php echo $alt; ?>" />
+        </div>
+      </div> <!-- /.media-block__image-wrap -->
     <?php endif; ?>
     <div class="media-block__content <?php if ($thumbnail): ?>block__content<?php endif; ?>">
-      <h3 class="media-block__title block__title <?php if (isset($title_size)): echo $title_size; endif; ?> "><a href="<?php echo $button_url; ?>" class="block__title-link theme--primary-text-color"><?php if (!empty($title)): echo $title; else: echo $kicker; endif; ?></a></h3>
+      <h3 class="media-block__title block__title <?php if (isset($title_size)): echo $title_size; endif; ?> ">
+        <?php if ($button_url): ?>
+          <a href="<?php echo $button_url; ?>" class="block__title-link theme--primary-text-color"><?php if (!empty($title)): echo $title; else: echo $kicker; endif; ?></a>
+        <?php else: ?>
+          <span class="theme--primary-text-color"><?php if (!empty($title)): echo $title; else: echo $kicker; endif; ?></span>
+        <?php endif; ?>
+      </h3>
       <?php if (isset($date)): ?>
         <time class="block__date font--secondary--xs brown space-half--btm" datetime="<?php echo $date_formatted; ?>"><?php echo $date; ?></time>
       <?php endif; ?>
