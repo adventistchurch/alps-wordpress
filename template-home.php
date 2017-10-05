55df73ab1091dad6dd794c1200f05a12c7c60cb5
Kelsey Cahill <kcahill@southleft.com>
2017-10-05 08:36:58 -0400

    #141 Extra Blank Space with no page title or content

--- template-home.php
+++ template-home.php
@@ -10,18 +10,20 @@
       <div class="shift-left--fluid column__primary bg--white can-be--dark-light no-pad--btm">
         <div class="spacing--double flex h--100p">
           <?php if (is_active_sidebar('widget_area_primary_top')): ?>
-              <div class="pad--primary spacing text">
+            <div class="pad--primary spacing text">
               <?php dynamic_sidebar('widget_area_primary_top'); ?>
             </div>
           <?php endif; ?>
-          <div class="pad--primary spacing text">
-            <?php if (get_post_meta($post->ID, 'display_title', true)): ?>
-              <h2 class="font--tertiary--l theme--primary-text-color">
-                <?php echo get_post_meta($post->ID, 'display_title', true); ?>
-              </h2>
-            <?php endif; ?>
-            <?php the_content(); ?>
-          </div>
+          <?php if (get_post_meta($post->ID, 'display_title', true) || get_the_content()): ?>
+            <div class="pad--primary spacing text">
+              <?php if (get_post_meta($post->ID, 'display_title', true)): ?>
+                <h2 class="font--tertiary--l theme--primary-text-color">
+                  <?php echo get_post_meta($post->ID, 'display_title', true); ?>
+                </h2>
+              <?php endif; ?>
+              <?php the_content(); ?>
+            </div>
+          <?php endif; ?>
           <?php if (is_active_sidebar('widget_area_primary')): ?>
             <div class="with-divider grid--uniform">
               <?php dynamic_sidebar('widget_area_primary'); ?>
