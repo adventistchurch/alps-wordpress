a36e8a619686c46914f4ce19cb232cfb8bc93f84
Kelsey Cahill <kcahill@southleft.com>
2017-10-06 13:51:46 -0400

    Removed empty div

--- template-home.php
+++ template-home.php
@@ -25,14 +25,14 @@
             </div>
           <?php endif; ?>
           <?php if (is_active_sidebar('widget_area_primary')): ?>
-            <div class="with-divider grid--uniform">
+            <div class="widget with-divider grid--uniform">
               <?php dynamic_sidebar('widget_area_primary'); ?>
             </div>
           <?php endif; ?>
           <?php include(locate_template('templates/block-layout-front.php')); ?>
           <?php include(locate_template('patterns/blocks/block-story.php')); ?>
           <?php if (is_active_sidebar('widget_area_primary_bottom')): ?>
-            <div class="pad--primary spacing text">
+            <div class="widget pad--primary spacing text">
               <?php dynamic_sidebar('widget_area_primary_bottom'); ?>
             </div>
           <?php endif; ?>
