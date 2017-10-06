8cc25242d4ec719963815fda6e90c2f196f9efc3
Kelsey Cahill <kcahill@southleft.com>
2017-10-06 10:39:16 -0400

    #138 Internationalized Logos

--- piklist/parts/settings/settings-theme.php
+++ piklist/parts/settings/settings-theme.php
@@ -6,44 +6,91 @@
   Sub Tab: Advanced
   Setting: alps_theme_settings
 */
-  piklist('field', array(
-    'type' => 'file',
-    'field' => 'logo_desktop',
-    'label' => 'Logo Desktop',
-    'description' => 'Upload the default logo that will be used at the large breakpoints',
-    'options' => array(
-      'modal_title' => 'Upload Image',
-      'button' => 'Add Image'
-    )
-  ));
+
   piklist('field', array(
     'type' => 'checkbox',
     'field' => 'logo_desktop_wide',
-    'label' => 'Logo Desktop (Wide)',
+    'label' => 'Logo Desktop Wide',
     'choices' => array(
       'wide' => 'Select if the default logo orientation is wide rather than square.'
     )
   ));
-  piklist('field', array(
-    'type' => 'file',
-    'field' => 'logo_mobile',
-    'label' => 'Logo Mobile',
-    'description' => 'Upload the horizontal format of the logo that will be used at the small breakpoints',
-    'options' => array(
-      'modal_title' => 'Upload Image',
-      'button' => 'Add Image'
-    )
-  ));
-  piklist('field', array(
-    'type' => 'file',
-    'field' => 'logo_text',
-    'label' => 'Logo Text',
-    'description' => 'Uploaded an image of the text to appear below the main logo',
-    'options' => array(
-      'modal_title' => 'Upload Image',
-      'button' => 'Add Image'
-    )
-  ));
+  $languages = apply_filters('wpml_active_languages', NULL);
+  if (!empty($languages)) {
+    foreach ($languages as $language) {
+      piklist('field', array(
+        'type' => 'file',
+        'field' => 'logo_desktop_' . $language['code'],
+        'label' => 'Logo Desktop (' . $language['translated_name'] . ')',
+        'description' => 'Upload the default ' . $language['translated_name'] . ' logo for the that will be used at the large breakpoints',
+        'options' => array(
+          'modal_title' => 'Upload Image',
+          'button' => 'Add Image'
+        )
+      ));
+    }
+  } else {
+    piklist('field', array(
+      'type' => 'file',
+      'field' => 'logo_desktop',
+      'label' => 'Logo Desktop',
+      'description' => 'Upload the default logo that will be used at the large breakpoints',
+      'options' => array(
+        'modal_title' => 'Upload Image',
+        'button' => 'Add Image'
+      )
+    ));
+  }
+  if (!empty($languages)) {
+    foreach ($languages as $language) {
+      piklist('field', array(
+        'type' => 'file',
+        'field' => 'logo_mobile_' . $language['code'],
+        'label' => 'Logo Mobile (' . $language['translated_name'] . ')',
+        'description' => 'Upload the horizontal format of the ' . $language['translated_name'] . ' logo that will be used at the small breakpoints',
+        'options' => array(
+          'modal_title' => 'Upload Image',
+          'button' => 'Add Image'
+        )
+      ));
+    }
+  } else {
+    piklist('field', array(
+      'type' => 'file',
+      'field' => 'logo_mobile',
+      'label' => 'Logo Mobile',
+      'description' => 'Upload the horizontal format of the logo that will be used at the small breakpoints',
+      'options' => array(
+        'modal_title' => 'Upload Image',
+        'button' => 'Add Image'
+      )
+    ));
+  }
+  if (!empty($languages)) {
+    foreach ($languages as $language) {
+      piklist('field', array(
+        'type' => 'file',
+        'field' => 'logo_text_' . $language['code'],
+        'label' => 'Logo Text (' . $language['translated_name'] . ')',
+        'description' => 'Uploaded an image of the ' . $language['translated_name'] . ' text to appear below the main logo',
+        'options' => array(
+          'modal_title' => 'Upload Image',
+          'button' => 'Add Image'
+        )
+      ));
+    }
+  } else {
+    piklist('field', array(
+      'type' => 'file',
+      'field' => 'logo_text',
+      'label' => 'Logo Text',
+      'description' => 'Uploaded an image of the text to appear below the main logo',
+      'options' => array(
+        'modal_title' => 'Upload Image',
+        'button' => 'Add Image'
+      )
+    ));
+  }
   piklist('field', array(
     'type' => 'select',
     'field' => 'primary_theme_color',
