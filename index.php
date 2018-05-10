48c3e7740b5c549aa053667ac0e1a62f8ba50e49
Kelsey Cahill <kcahill@southleft.com>
2018-05-10 14:54:10 -0400

    Moved the repo to themes folder for v3

--- index.php
+++ index.php
@@ -1,5 +1,2 @@
-<?php get_template_part('templates/page', 'header'); ?>
-<?php if (is_page_template('template-news.php')): ?>
-  <?php include(locate_template('patterns/components/news-navigation.php')); ?>
-<?php endif; ?>
-<?php get_template_part('templates/content'); ?>
+<?php
+// Silence is golden.
