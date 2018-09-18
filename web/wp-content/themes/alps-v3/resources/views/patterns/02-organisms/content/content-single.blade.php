64654a7b5b328f85ace13be57d75fd2af649d5bf
Kelsey Cahill <kcahill@southleft.com>
2018-09-18 11:49:09 -0400

    #203 Gutenberg Blocks: Disable Dropcap

--- web/wp-content/themes/alps-v3/resources/views/patterns/02-organisms/content/content-single.blade.php
+++ web/wp-content/themes/alps-v3/resources/views/patterns/02-organisms/content/content-single.blade.php
@@ -7,11 +7,17 @@
     $section_offset = 'u-shift--left--1-col--at-large';
     $article_offset = '';
   }
+
+  if (get_post_meta($post->ID, 'hide_dropcap', true) != 'true') {
+    $classes = "has-dropcap";
+  } else {
+    $classes = "";
+  }
 @endphp
 @include('patterns.02-organisms.sections.page-header-feature')
 <section class="l-grid l-grid--7-col {{ $section_offset }} l-grid-wrap--6-of-7 u-spacing--double--until-large">
   <div class="c-article l-grid-item l-grid-item--l--4-col {{ $article_offset }}">
-    <article @php post_class('text c-article__body u-spacing--double has-dropcap') @endphp>
+    <article @php post_class("text c-article__body u-spacing--double $classes") @endphp>
       @php the_content() @endphp
       @include('patterns.02-organisms.sections.article-footer')
     </article>
