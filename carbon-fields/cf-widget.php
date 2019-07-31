5a7774bcea4dbedb0002caf4e98fd64dda8c78e8
David Eleuterius <davideleuterius@gmail.com>
2019-07-31 15:19:45 -0500

    adding CF widgets
    This replicates & replaces the existing widgets from Piklist.

--- carbon-fields/cf-widget.php
+++ carbon-fields/cf-widget.php
@@ -3,12 +3,62 @@
 use Carbon_Fields\Widget;
 use Carbon_Fields\Field;
 
-class ThemeWidgetExample extends Widget {
+
+
+class ALPS_Social_Widget extends Widget {
     // Register widget function. Must have the same name as the class
     function __construct() {
-        $this->setup( 'theme_widget_example', 'Theme Widget - Example', 'Displays a block with title/text', array(
-            Field::make( 'text', 'title', 'Title' )->set_default_value( 'Hello World!') ,
-            Field::make( 'textarea', 'content', 'Content' )->set_default_value( 'Lorem Ipsum dolor sit amet' )
+        $this->setup( 'alps_widget_social', 'ALPS - Social Links', 'Set your social links.', array(
+            Field::make( 'text', 'facebook_url', 'Facebook URL' ) ,
+            Field::make( 'text', 'twitter_url', 'Twiiter URL' ) ,
+            Field::make( 'text', 'flickr_url', 'Flickr URL' ) ,
+            Field::make( 'text', 'youtube_url', 'YouTube URL' ) ,
+            Field::make( 'text', 'vimeo_url', 'Vimeo URL' ) ,
+            Field::make( 'text', 'email_address', 'Email Address' ) ,
+            Field::make( 'checkbox', 'horizontal_rule', __( 'Add a horizontal rule above the block' ) )
+        ) );
+    }
+
+    // Called when rendering the widget in the front-end
+    function front_end( $args, $instance ) {
+        echo $args['before_title'] . $instance['title'] . $args['after_title'];
+        echo '<p>' . $instance['content'] . '</p>';
+    }
+}
+
+class ALPS_Text_With_Link_Widget extends Widget {
+    // Register widget function. Must have the same name as the class
+    function __construct() {
+        $this->setup( 'alps_widget_text_with_link', 'ALPS - Text with Link', 'Text block with formatted link.', array(
+            Field::make( 'text', 'title', 'Title' ) ,
+            Field::make( 'textarea', 'content', __( 'Content' ) ) ,
+            Field::make( 'text', 'url', 'URL' ) 
+                ->set_width( 50 ),
+            Field::make( 'text', 'url_text', 'URL Text' )
+                ->set_width( 50 )
+        ) );
+    }
+
+    // Called when rendering the widget in the front-end
+    function front_end( $args, $instance ) {
+        echo $args['before_title'] . $instance['title'] . $args['after_title'];
+        echo '<p>' . $instance['content'] . '</p>';
+    }
+}
+
+class ALPS_Post_Feed_Widget extends Widget {
+    // Register widget function. Must have the same name as the class
+    function __construct() {
+        $this->setup( 'alps_widget_post_feed', 'ALPS - Post Feed', 'Feed of posts in the selected caetgory.', array(
+            Field::make( 'select', 'feed_category_list', __( 'Feed Category' ) )
+                ->set_options( cat_list() ),
+            Field::make( 'text', 'feed_title', 'Feed Title' ) ,
+            Field::make( 'text', 'feed_widget_post_count', 'Number of Posts' ) ,
+            Field::make( 'checkbox', 'for_sidebar', __( ' Format For Sidebar?' ) ),
+            Field::make( 'text', 'feed_widget_btn_text', 'More Button Text' )
+                ->set_width( 50 ),
+            Field::make( 'text', 'feed_widget_btn_link', 'More Button Link' ) 
+                ->set_width( 50 )
         ) );
     }
 
@@ -21,5 +71,17 @@ class ThemeWidgetExample extends Widget {
 
 add_action( 'widgets_init', 'alps_widgets' );
 function alps_widgets() {
-    register_widget( 'ThemeWidgetExample' );
+    register_widget( 'ALPS_Social_Widget' );
+    register_widget( 'ALPS_Text_With_Link_Widget' );
+    register_widget( 'ALPS_Post_Feed_Widget' );
+}
+
+function cat_list() {
+    // NEED TO BUILD CUSTOM CATEGORY LIST HERE DUE TO CARBON FIELDS ERROR
+    $categories = get_categories( array( 'orderby' => 'name' ) );
+    $cats = [];
+    foreach ( $categories as $cat ) {
+      $cats[ $cat->term_id ] = $cat->name;
+    }
+    return $cats;
 }
