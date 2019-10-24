b5638a915311a32b0546dd591737ffe94df5eb74
Kelsey Cahill <kcahill@southleft.com>
2019-10-24 11:46:38 -0400

    #348 Fix for rel links and added conditionals to attributes.

--- views/patterns/01-molecules/navigation/footer-secondary-navigation.blade.php
+++ views/patterns/01-molecules/navigation/footer-secondary-navigation.blade.php
@@ -8,8 +8,17 @@
       $footer_secondary_nav = json_decode(json_encode($footer_secondary_nav), true);
     @endphp
     @foreach ($footer_secondary_nav as $nav)
-      <a href="{{ $nav['url'] }}" class="c-footer__secondary-nav__link u-theme--link-hover--light u-link--white {{ implode(' ', $nav['classes']) }}" target="{{ $nav['target'] }}" title="{{ $nav['attr_title'] }}" description="{{ $nav['description'] }}" rel="{{ implode(' ', $nav['xfn']) }}">
-        <span class="u-icon u-icon--xs u-path-fill--white u-space--half--right">@include('patterns.00-atoms.icons.icon-legal')</span><font>{{ $nav['title'] }}</font>
+      @php
+        $link_url = $nav['url'];
+        $link_text = $nav['title'];
+        $link_classes = ($nav['classes'] ? ' ' . implode(' ', $nav['classes']) : '');
+        $link_target = ($nav['target'] ? ' target="'. $nav['target'] . '"' : '');
+        $link_title = ($nav['attr_title'] ? ' title="'. $nav['attr_title'] . '"' : '');
+        $link_description = ($nav['description'] ? ' description="' . $nav['description'] . '"' : '');
+        $link_rel = ($nav['xfn'] ? ' rel="'. $nav['xfn'] . '"' : '');
+      @endphp
+      <a href="{{ $link_url }}" class="c-footer__secondary-nav__link u-theme--link-hover--light u-link--white{{ $link_classes }}"{!! $link_target !!}{!! $link_title !!}{!! $link_description !!}{!! $link_rel !!}>
+        <span class="u-icon u-icon--xs u-path-fill--white u-space--half--right">@include('patterns.00-atoms.icons.icon-legal')</span><font>{{ $link_text }}</font>
       </a>
     @endforeach
     {!! wp_reset_postdata() !!}
--- views/patterns/01-molecules/navigation/primary-navigation.blade.php
+++ views/patterns/01-molecules/navigation/primary-navigation.blade.php
@@ -7,12 +7,19 @@
       $primary_nav = wp_get_nav_menu_items( $menu->term_id);
       $count = 0;
       $submenu = false;
-
     @endphp
     <ul class="c-primary-nav__list c-priority-nav__list">
-      @php
-        $primary_nav = json_decode(json_encode($primary_nav), true); @endphp
+      @php $primary_nav = json_decode(json_encode($primary_nav), true); @endphp
       @foreach ($primary_nav as $nav)
+        @php
+          $link_url = $nav['url'];
+          $link_text = $nav['title'];
+          $link_classes = ($nav['classes'] ? ' ' . implode(' ', $nav['classes']) : '');
+          $link_target = ($nav['target'] ? ' target="'. $nav['target'] . '"' : '');
+          $link_title = ($nav['attr_title'] ? ' title="'. $nav['attr_title'] . '"' : '');
+          $link_description = ($nav['description'] ? ' description="' . $nav['description'] . '"' : '');
+          $link_rel = ($nav['xfn'] ? ' rel="'. $nav['xfn'] . '"' : '');
+        @endphp
         @if (isset($primary_nav[$count + 1]))
           @php
             $parent = $primary_nav[$count + 1]['menu_item_parent'];
@@ -21,7 +28,7 @@
         @if (!$nav['menu_item_parent'])
           @php $parent_id = $nav['ID']; @endphp
           <li class="c-primary-nav__list-item has-subnav">
-            <a href="{{ $nav['url'] }}" class="c-primary-nav__link u-font--primary-nav u-color--gray--dark u-theme--link-hover--base u-theme--border-color--base {{ implode(' ', $nav['classes']) }}" target="{{ $nav['target'] }}" title="{{ $nav['attr_title'] }}" description="{{ $nav['description'] }}" rel="{{ implode(' ', $nav['xfn']) }}">{{ $nav['title'] }}</a>
+            <a href="{{ $link_url }}" class="c-primary-nav__link u-font--primary-nav u-color--gray--dark u-theme--link-hover--base u-theme--border-color--base{{ $link_classes }}"{!! $link_target !!}{!! $link_title !!}{!! $link_description !!}{!! $link_rel !!}>{{ $link_text }}</a>
         @endif
         @if ($parent_id == $nav['menu_item_parent'])
           @if (!$submenu)
@@ -30,7 +37,7 @@
             <ul class="c-primary-nav__subnav c-subnav">
           @endif
             <li class="c-primary-nav__subnav__list-item c-subnav__list-item u-background-color--gray--light">
-              <a href="{{ $nav['url'] }}" class="c-primary-nav__subnav__link c-subnav__link u-color--gray--dark u-theme--link-hover--base {{ implode(' ', $nav['classes']) }}" target="{{ $nav['target'] }}" title="{{ $nav['attr_title'] }}" description="{{ $nav['description'] }}" rel="{{ implode(' ', $nav['xfn']) }}">{{ $nav['title'] }}</a>
+              <a href="{{ $link_url }}" class="c-primary-nav__subnav__link c-subnav__link u-color--gray--dark u-theme--link-hover--base{{ $link_classes }}"{!! $link_target !!}{!! $link_title !!}{!! $link_description !!}{!! $link_rel !!}>{{ $link_text }}</a>
             </li>
             @if ($parent != $parent_id && $submenu)
               </ul> <!-- /.c-primary-nav__subnav -->
