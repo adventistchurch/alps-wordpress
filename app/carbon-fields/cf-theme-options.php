9bd6c90cf938c1fd873bb7bbf069f69c964e5a54
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2020-11-05 15:12:01 +0200

    fix: place Wide Logo checkbox in one line with logo picker

--- app/carbon-fields/cf-theme-options.php
+++ app/carbon-fields/cf-theme-options.php
@@ -61,11 +61,6 @@ function crb_attach_theme_options()
     $languages = apply_filters('wpml_active_languages', NULL);
     $logoFields = [
         Field::make('separator', 'crb_logo', __('Logo', 'alps')),
-        Field
-            ::make('checkbox', 'is_wide_logo', __('Wide Logo', 'alps'))
-            ->set_option_value('true')
-            ->set_help_text(__('Select if you would like to use a wider than normal logo.', 'alps'))
-            ->set_width(100),
     ];
     if (empty($languages)) {
         $logoFields[] = Field::make('image', 'logo', __('Logo', 'alps'));
@@ -76,6 +71,11 @@ function crb_attach_theme_options()
                 ->set_width(33);
         }
     }
+    $logoFields[] = Field
+        ::make('checkbox', 'is_wide_logo', __('Wide Logo', 'alps'))
+        ->set_option_value('true')
+        ->set_help_text(__('Select if you would like to use a wider than normal logo.', 'alps'))
+        ->set_width(33);
 
     $versionFields = [];
     $versions = \App\Core\ALPSVersions::getAll();
