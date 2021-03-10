3c71b9d33bf91926b07fe20fa77f938a8fd89f8f
Brent Hardinge <designerbrent@gmail.com>
2021-03-10 09:54:57 -0500

    Fix to related stories and drawer h3's to use div's instead. Related to #558.

--- views/patterns/02-organisms/asides/related-stories.blade.php
+++ views/patterns/02-organisms/asides/related-stories.blade.php
@@ -30,7 +30,7 @@
 
 <div class="c-related-posts u-spacing">
   <div class="c-block__heading u-theme--border-color--darker">
-    <h3 class="c-block__heading-title u-theme--color--darker">{{ __('Related Stories', 'alps') }}</h3>
+    <div class="c-block__heading-title u-theme--color--darker">{{ __('Related Stories', 'alps') }}</div>
   </div>
   <div class="c-related-posts__blocks u-spacing">
     @if ($related->have_posts())
