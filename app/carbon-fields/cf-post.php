22781f781446736100837fd29a6db262a58fdc3b
YauheniKapliarchuk <Yauheni.Kapliarchuk@gmail.com>
2021-04-21 22:13:44 +0300

    [#548] Merged with latest version of v3.

--- app/carbon-fields/cf-post.php
+++ app/carbon-fields/cf-post.php
@@ -31,27 +31,19 @@ function crb_attach_post_template_fields()
         ->add_fields([
             Field
                 ::make('separator', 'crb_hide_sidebar', __('Hide Sidebar', 'alps'))
-                ->set_width(33),
+                ->set_width(50),
             Field
                 ::make('separator', 'crb_hide_img', __('Hide Featured Image', 'alps'))
-                ->set_width(33),
-            Field
-                ::make('separator', 'crb_show_related_stories', __('Show related stories on Post', 'alps'))
-                ->set_width(33),
+                ->set_width(50),
             Field
                 ::make('checkbox', 'hide_sidebar', __('Hide Sidebar', 'alps'))
                 ->set_help_text(__('Hides the sidebar for this entry if it is active.', 'alps'))
                 ->set_option_value('true')
-                ->set_width(33),
+                ->set_width(50),
             Field
                 ::make('checkbox', 'hide_featured_image', __('Hide Featured Image', 'alps'))
                 ->set_help_text(__('Hides the featured image on the page/post header for this entry.', 'alps'))
                 ->set_option_value('true')
-                ->set_width(33),
-            Field
-                ::make('checkbox', 'display_related_stories', __('Show related stories on Post', 'alps'))
-                ->set_help_text(__('Hide (default option and managed in theme settings) / show Related Stories on the Post Page.', 'alps'))
-                ->set_option_value('false')
-                ->set_width(33),
+                ->set_width(50),
         ]);
 }
--- views/patterns/02-organisms/asides/related-stories.blade.php
+++ views/patterns/02-organisms/asides/related-stories.blade.php
@@ -27,63 +27,54 @@
     'post__not_in' => array($post->ID)
   );
   $related = new WP_Query($args);
-
-  global $post;
-
-  $isGlobalHidingRelatedStories = get_alps_option('project_alps_related_stories_is_hiding_on_post_page');
-  $showRelatedStoriesOnPostPage = get_post_meta($post->ID, $cf_.'display_related_stories', true);
 @endphp
 
-@if ((!$isGlobalHidingRelatedStories && $showRelatedStoriesOnPostPage) ||
-      ($isGlobalHidingRelatedStories && $showRelatedStoriesOnPostPage)
-)
-  <div class="c-related-posts u-spacing">
-    <div class="c-block__heading u-theme--border-color--darker">
-      <h3 class="c-block__heading-title u-theme--color--darker">{{ __('Related Stories', 'alps') }}</h3>
-    </div>
-    <div class="c-related-posts__blocks u-spacing">
-      @if ($related->have_posts())
-        @while ($related->have_posts())
-          @php
-            $related->the_post();
-            $id = get_the_ID();
-            $title = get_the_title();
-            $link = get_permalink();
-            $date = get_the_date('F j, Y');
-            $category = $category;
-          @endphp
-          @if ($isVisibleImage && get_post_thumbnail_id())
-            @php
-              $thumb_id = get_post_thumbnail_id();
-              $thumb_size = 'horiz__4x3';
-              $image = wp_get_attachment_image_src($thumb_id, $thumb_size . '--s')[0];
-              $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
-              $block_class = "c-block--reversed c-media-block--reversed l-grid--7-col";
-              $block_title_class = "u-theme--color--darker u-font--primary--s";
-              $block_meta_class = "u-theme--color--dark u-font--secondary--xs";
-              $block_group_class = "u-flex--justify-start";
-              $block_content_class = "l-grid-item--4-col l-grid-item--m--3-col l-grid-item--l--1-col u-border--left u-theme--border-color--darker--left u-color--gray u-spacing--half";
-              $block_img_class = "l-grid-item--2-col l-grid-item--m--1-col l-grid-item--l--1-col u-padding--right";
-              $title_div = true;
-            @endphp
-            @include('patterns.01-molecules.blocks.media-block')
-          @else
-            @php
-              $thumb_id = NULL;
-              $block_class = "c-block__text u-theme--border-color--darker u-border--left u-padding--bottom u-padding--right u-spacing--half";
-              $block_title_class = "u-theme--color--darker u-font--primary--s";
-              $excerpt = get_the_excerpt();
-              $body = get_the_content();
-              $excerpt_length = 35;
-              $title_div = true;
-            @endphp
-            @include('patterns.01-molecules.blocks.content-block')
-          @endif
-        @endwhile
-        {!! wp_reset_postdata() !!}
-      @else
-        {{ __('There are no related stories at this time.', 'alps') }}
-      @endif
-    </div>
+<div class="c-related-posts u-spacing">
+  <div class="c-block__heading u-theme--border-color--darker">
+    <h3 class="c-block__heading-title u-theme--color--darker">{{ __('Related Stories', 'alps') }}</h3>
   </div>
-@endif
+  <div class="c-related-posts__blocks u-spacing">
+    @if ($related->have_posts())
+      @while ($related->have_posts())
+        @php
+          $related->the_post();
+          $id = get_the_ID();
+          $title = get_the_title();
+          $link = get_permalink();
+          $date = get_the_date('F j, Y');
+          $category = $category;
+        @endphp
+        @if ($isVisibleImage && get_post_thumbnail_id())
+          @php
+            $thumb_id = get_post_thumbnail_id();
+            $thumb_size = 'horiz__4x3';
+            $image = wp_get_attachment_image_src($thumb_id, $thumb_size . '--s')[0];
+            $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
+            $block_class = "c-block--reversed c-media-block--reversed l-grid--7-col";
+            $block_title_class = "u-theme--color--darker u-font--primary--s";
+            $block_meta_class = "u-theme--color--dark u-font--secondary--xs";
+            $block_group_class = "u-flex--justify-start";
+            $block_content_class = "l-grid-item--4-col l-grid-item--m--3-col l-grid-item--l--1-col u-border--left u-theme--border-color--darker--left u-color--gray u-spacing--half";
+            $block_img_class = "l-grid-item--2-col l-grid-item--m--1-col l-grid-item--l--1-col u-padding--right";
+            $title_div = true;
+          @endphp
+          @include('patterns.01-molecules.blocks.media-block')
+        @else
+          @php
+            $thumb_id = NULL;
+            $block_class = "c-block__text u-theme--border-color--darker u-border--left u-padding--bottom u-padding--right u-spacing--half";
+            $block_title_class = "u-theme--color--darker u-font--primary--s";
+            $excerpt = get_the_excerpt();
+            $body = get_the_content();
+            $excerpt_length = 35;
+            $title_div = true;
+          @endphp
+          @include('patterns.01-molecules.blocks.content-block')
+        @endif
+      @endwhile
+      {!! wp_reset_postdata() !!}
+    @else
+      {{ __('There are no related stories at this time.', 'alps') }}
+    @endif
+  </div>
+</div>
