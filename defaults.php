c0cb5d356ea8f037242513ff1d9826f75d31f747
John Petroviak <johnpetroviak@gmail.com>
2022-06-23 15:54:52 -0500

    New Defaults

--- defaults.php
+++ defaults.php
@@ -1,4 +1,21 @@
 <?php
-update_option( '_logo_nad', 'true' );
+// Custom Header logo file is located /patterns/00-atoms/logos/alps-logo-custom.blade.php
+
 update_option( '_sabbath_hide', 'true' );
-update_option( '_footer_logo_square', 'true' );
+update_option( '_footer_logo_type', 'square' );
+
+$menu = get_term_by('name', 'Learn More', 'nav_menu');
+wp_update_nav_menu_item($menu->term_id, 0, array(
+  'menu-item-title' =>  __('Google', 'alps'),
+  'menu-item-classes' => '',
+  'menu-item-url' => 'https://www.google.com/',
+  'menu-item-status' => 'publish'
+));
+
+$menu = get_term_by('name', 'Footer Secondary Navigation', 'nav_menu');
+wp_update_nav_menu_item($menu->term_id, 0, array(
+  'menu-item-title' =>  __('Google', 'alps'),
+  'menu-item-classes' => '',
+  'menu-item-url' => 'https://www.google.com/',
+  'menu-item-status' => 'publish'
+));
