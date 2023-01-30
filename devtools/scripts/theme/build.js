e6c83d1c842e154d9db542dc82c20fed56081b58
yauhenikapliarchuk <yauheni.kapliarchuk@gmail.com>
2023-01-30 19:01:16 +0300

    #628-New BUILD Infrastructure of ALPS Theme. Update build script. Add steps.

--- devtools/scripts/theme/build.js
+++ devtools/scripts/theme/build.js
@@ -7,6 +7,80 @@ const dirTree = require('../../lib/dir-tree');
 const getPackageInfo = require('../../lib/get-package-info');
 const getThemeMeta = require('../../lib/get-theme-meta');
 
+const themeBuild = async (opts) => {
+  const { logger, args } = opts;
+
+  const pkg = await getPackageInfo();
+
+  const buildDir = `build/`;
+  const pluginDir = `${buildDir}${pkg.name}/`;
+  await fs.emptyDir(buildDir);
+  await fs.emptyDir(pluginDir);
+
+  if (args.dev) {
+    logger.info('🎯 Build theme in dev mode');
+  } else {
+    logger.info('🎯 Build ALPS Theme');
+  }
+
+  await exec('sudo apt-get install php php-curl', logger);
+  await exec('curl -sS https://getcomposer.org/installer -o composer-setup.php', logger);
+  await exec('sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer', logger);
+  await exec('sudo composer self-update', logger);
+  await exec('composer install', logger);
+
+  logger.info(`💼 Copy plugin files to ${chalk.yellow(pluginDir)}`);
+
+  const whiteList = [
+    /^\/app\/.+\.php$/u,
+    /^\/app\/carbon-fields\/.+\.js$/u,
+    /^\/app\/carbon-fields\/.+\.css$/u,
+    /^\/app\/local\/.+\.md$/u,
+    /^\/public\/.+$/u,
+    /^\/[^\/]+\.php$/u,
+    /^\/[^\/]+\.json$/u,
+    /^\/[^\/]+\.js$/u,
+    /^\/[^\/]+\.md$/u,
+    /^\/[^\/]+\.png$/u,
+    /^\/[^\/]+\.css$/u,
+    /^\/[^\/]+\.lock$/u,
+    /^\.editorconfig$/u,
+    /^\/dist/u,
+    /^\/vendor/u,
+    /^\/resources/u,
+  ];
+  const pluginFiles = await dirTree('.', {
+    whiteList,
+  });
+
+  for (const pf of pluginFiles) {
+    await fs.copy(`./${pf}`, `${pluginDir}${pf}`);
+  }
+
+  // Package plugin
+  if (!args.dev) {
+    await createArchive(buildDir, pkg.name);
+    logger.info(`💚 ALPS Theme packaged to ${chalk.yellow(`${pkg.name}.zip`)}`);
+  }
+
+  // Gather metadata
+  // if (!args.dev) {
+  //     const themeMeta = {
+  //         ...await getThemeMeta(),
+  //         version: pkg.version,
+  //         last_updated: DateTime.utc().toFormat('yyyy-LL-dd HH:mm:ss ZZZZ'),
+  //     };
+  //
+  //     themeMeta.download_url = themeMeta.download_url
+  //         .replace('{name}', pkg.name)
+  //         .replace('{file}', `${pkg.name}-v${pkg.version}.zip`);
+  //
+  //     await fs.writeFile(`${buildDir}${pkg.name}.json`, JSON.stringify(themeMeta, null, 2));
+  //
+  //     logger.info(`💚 ALPS Theme metadata saved to ${chalk.yellow(`${pkg.name}.json`)}`);
+  // }
+}
+
 const createArchive = (src, name, logger) => {
     return new Promise((resolve, reject) => {
         const archiveOutput = fs.createWriteStream(`${src}${name}.zip`);
@@ -37,75 +111,5 @@ const createArchive = (src, name, logger) => {
         archive.finalize();
     });
 };
-
-const themeBuild = async (opts) => {
-    const { logger, args } = opts;
-
-    const pkg = await getPackageInfo();
-
-    const buildDir = `build/`;
-    const pluginDir = `${buildDir}${pkg.name}/`;
-    await fs.emptyDir(buildDir);
-    await fs.emptyDir(pluginDir);
-
-    if (args.dev) {
-        logger.info('🎯 Build theme in dev mode');
-    } else {
-        logger.info('🎯 Build ALPS Theme');
-    }
-
-    await exec('composer install', logger);
-
-    logger.info(`💼 Copy plugin files to ${chalk.yellow(pluginDir)}`);
-
-    const whiteList = [
-        /^\/app\/.+\.php$/u,
-        /^\/app\/carbon-fields\/.+\.js$/u,
-        /^\/app\/carbon-fields\/.+\.css$/u,
-        /^\/app\/local\/.+\.md$/u,
-        /^\/public\/.+$/u,
-        /^\/[^\/]+\.php$/u,
-        /^\/[^\/]+\.json$/u,
-        /^\/[^\/]+\.js$/u,
-        /^\/[^\/]+\.md$/u,
-        /^\/[^\/]+\.png$/u,
-        /^\/[^\/]+\.css$/u,
-        /^\/[^\/]+\.lock$/u,
-        /^\.editorconfig$/u,
-        /^\/dist/u,
-        /^\/vendor/u,
-        /^\/resources/u,
-    ];
-    const pluginFiles = await dirTree('.', {
-        whiteList,
-    });
-
-    for (const pf of pluginFiles) {
-        await fs.copy(`./${pf}`, `${pluginDir}${pf}`);
-    }
-
-    // Package plugin
-    if (!args.dev) {
-        await createArchive(buildDir, pkg.name);
-        logger.info(`💚 ALPS Theme packaged to ${chalk.yellow(`${pkg.name}.zip`)}`);
-    }
-
-    // Gather metadata
-    // if (!args.dev) {
-    //     const themeMeta = {
-    //         ...await getThemeMeta(),
-    //         version: pkg.version,
-    //         last_updated: DateTime.utc().toFormat('yyyy-LL-dd HH:mm:ss ZZZZ'),
-    //     };
-    //
-    //     themeMeta.download_url = themeMeta.download_url
-    //         .replace('{name}', pkg.name)
-    //         .replace('{file}', `${pkg.name}-v${pkg.version}.zip`);
-    //
-    //     await fs.writeFile(`${buildDir}${pkg.name}.json`, JSON.stringify(themeMeta, null, 2));
-    //
-    //     logger.info(`💚 ALPS Theme metadata saved to ${chalk.yellow(`${pkg.name}.json`)}`);
-    // }
-}
 
 module.exports = themeBuild;
