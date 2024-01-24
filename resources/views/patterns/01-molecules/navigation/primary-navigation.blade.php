0eee1041e145175aa034c5f612ba549f2ba6cd5a
Yauheni Kapliarchuk <@YauheniKapliarchuk>
2024-01-24 13:25:28 +0300

    Primary Navigation Menu - issue with subitems
    Changes:
    - Add new level for menu;
    - Update styles

--- resources/views/patterns/01-molecules/navigation/primary-navigation.blade.php
+++ resources/views/patterns/01-molecules/navigation/primary-navigation.blade.php
@@ -50,6 +50,7 @@
                       @php
                         $parentID = $sub->ID;
                         $sub_subitems = array_filter($nav_items, function($item) use ($parentID) { return $item->menu_item_parent == $parentID; });
+                        $third_level_toggleId = 0;
                       @endphp
                       @foreach ($sub_subitems as $sub_subitem => $sub_item)
                         @php
@@ -60,9 +61,36 @@
                           $link_title = ($sub_item->attr_title ? ' title="'. $sub_item->attr_title . '"' : '');
                           $link_description = ($sub_item->description ? ' description="' . $sub_item->description . '"' : '');
                           $link_rel = ($sub_item->xfn ? ' rel="'. $sub_item->xfn . '"' : '');
+                          $show_3rd_level_subnav = '';
+                          $has_3rd_level_subnav = array_search($sub_item->ID, array_column($nav_items, 'menu_item_parent'));
+                          if ($has_3rd_level_subnav) $show_3rd_level_subnav = ' has_sub_subnav js-this';
+                          $third_toggled = 'third-toggled-' . $third_level_toggleId++;
                         @endphp
-                        <li class="c-primary-nav__subnav__subnav__list-item c-subnav__list-item u-theme--background-color--base">
+                        <li class="c-primary-nav__subnav__subnav__list-item c-subnav__list-item u-theme--background-color--base {{ $third_toggled }}{{ $show_3rd_level_subnav }}">
                           <a href="{{ $link_url }}" class="c-primary-nav__subnav__subnav__link c-subnav__link u-color--gray--dark u-theme--link-hover--lighter{{ $link_classes }}"{!! $link_target !!}{!! $link_title !!}{!! $link_description !!}{!! $link_rel !!}>{!! $link_text !!}</a>
+                          @if ($has_3rd_level_subnav)
+                            <span class="c-primary-nav__subnav__subnav__arrow c-subnav__arrow o-arrow--down u-path-fill--gray js-toggle" data-toggled="{{ $third_toggled }}" data-prefix="this"></span>
+                            <ul class="c-primary-nav__third-level">
+                              @php
+                                $parentID = $sub_item->ID;
+                                $last_level_subitems = array_filter($nav_items, function($item) use ($parentID) { return $item->menu_item_parent == $parentID; });
+                              @endphp
+                              @foreach ($last_level_subitems as $last_level_subitem => $last_item)
+                                @php
+                                  $link_url = $last_item->url;
+                                  $link_text = $last_item->title;
+                                  $link_classes = ($last_item->classes ? ' ' . implode(' ', $last_item->classes) : '');
+                                  $link_target = ($last_item->target ? ' target="'. $last_item->target . '"' : '');
+                                  $link_title = ($last_item->attr_title ? ' title="'. $last_item->attr_title . '"' : '');
+                                  $link_description = ($last_item->description ? ' description="' . $last_item->description . '"' : '');
+                                  $link_rel = ($last_item->xfn ? ' rel="'. $last_item->xfn . '"' : '');
+                                @endphp
+                                <li class="c-primary-nav__third-level__list-item c-subnav__list-item u-theme--background-color--base">
+                                  <a href="{{ $link_url }}" class="c-primary-nav__third-level__link c-subnav__link u-theme--link-hover--lighter{{ $link_classes }}"{!! $link_target !!}{!! $link_title !!}{!! $link_description !!}{!! $link_rel !!}>{!! $link_text !!}</a>
+                                </li>
+                            @endforeach
+                          </ul>
+                        @endif
                         </li>
                       @endforeach
                     </ul>
